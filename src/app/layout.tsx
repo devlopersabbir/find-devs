295a0ca5ee4d8d0571540e76b9eaad0515c43221
devlopersabbir <devlopersabbir@gmail.com>
2024-01-10 12:59:04 +0600

    [feat 🙋🏻‍♂️] comming soon message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Find Dev - Open source",
+
   description:
     "Find Dev is kind of Developers Fair. An open source platform to find all the developers in the world.",
+  creator: "Sabbir Hossain Shuvo",
+  publisher: "Sabbir Hossain Shuvo",
+  icons: "/favicon/favicon.ico",
+  authors: {
+    name: "Sabbir Hossain Shuvo",
+    url: "https://github.com/devlopersabbir",
+  },
 };
 
 export default function RootLayout({
@@ -16,8 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body
+        suppressHydrationWarning={true}
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-1 flex-grow">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-7xl text-gray-700 font-semibold">Find Devs</h1>
+      <p>Comming soon</p>
+    </div>
   );
 }
