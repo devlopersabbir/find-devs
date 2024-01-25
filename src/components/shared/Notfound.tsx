0be5bab8b404460f25efa89e129ec223464966aa
devlopersabbir <devlopersabbir@gmail.com>
2024-01-25 19:38:16 +0600

    [fix ✨] fix deployment issue

--- src/components/shared/Notfound.tsx
+++ src/components/shared/Notfound.tsx
@@ -7,7 +7,7 @@ const Notfound = () => {
       <Image src="/sorry.png" alt="not found" width={160} height={213} />
       <div className="flex-center flex-col gap-1">
         <h1 className="font-bold text-3xl">No Developers Founds!</h1>
-        <p className="font-light text-base">We couldn't find any developers</p>
+        <p className="font-light text-base">We couldnt find any developers</p>
       </div>
     </div>
   );
