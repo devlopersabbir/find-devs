25bc7811bae713a58c690283eb7c05ca3bd117f6
mx_lshk <max.loshak.i@gmail.com>
2024-03-18 20:10:27 +0200

    [fixed�] error: operator does not exist

--- src/components/contents/ProfileGrid.tsx
+++ src/components/contents/ProfileGrid.tsx
@@ -23,9 +23,12 @@ const ProfileGrid = async ({ page, searchParams }: Props) => {
   const currentPage = parseInt(page); // like 1
   const itemPerPage = 5; // we want to show 5 item in per pages
   const offset = (currentPage - 1) * itemPerPage; // (1 - 1) * 3 = 0
-  const searchSkills = searchParams ? searchParams.split(',') : [];
+  const searchSkills = searchParams ? searchParams.split(/[\s,]+/) : []; // regular expression to accept inputs separated by comma, space or both
 
-  const searchSkillsConditions = searchSkills.map(skill => ilike(users.skills, `%${skill}%`));
+  // In order to get rid of the "error: operator does not exist: json @> json" I mannually cast the skills column to JSONB
+  const skillsCondition = searchSkills.length > 0 
+  ? sql.raw(`"skills"::JSONB @> '${JSON.stringify(searchSkills)}'::JSONB`) 
+  : undefined;
 
   const [lengths, profiles] = await Promise.all([
     db.select({ count: sql<number>`count(*)` }).from(users),
@@ -37,7 +40,8 @@ const ProfileGrid = async ({ page, searchParams }: Props) => {
             or(
               ilike(users.name, `%${searchParams}%`),
               ilike(users.location, `%${searchParams}%`),
-              ...searchSkillsConditions
+              ilike(users.description, `%${searchParams}%`),
+              skillsCondition
             ),
           )
           .limit(itemPerPage)
