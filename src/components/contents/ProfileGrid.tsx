d85fe6d7cfd58844985ca6cad3f3a94ca7cad1e7
mx_lshk <max.loshak.i@gmail.com>
2024-03-18 14:45:32 +0200

    error: operator does not exist: json @> json fix

--- src/components/contents/ProfileGrid.tsx
+++ src/components/contents/ProfileGrid.tsx
@@ -3,7 +3,7 @@ import { db } from "@/database";
 import { users } from "@/schemas";
 import ProfileCard from "./ProfileCard";
 import Paginations from "../pagination/Paginations";
-import { ilike, or, sql } from "drizzle-orm";
+import { ilike, or, sql, arrayContains } from "drizzle-orm";
 import Notfound from "../shared/Notfound";
 import Search from "../shared/search/Search";
 
@@ -16,6 +16,9 @@ const ProfileGrid = async ({ page, searchParams }: Props) => {
   const currentPage = parseInt(page); // like 1
   const itemPerPage = 5; // we want to show 5 item in per pages
   const offset = (currentPage - 1) * itemPerPage; // (1 - 1) * 3 = 0
+  const searchSkills = searchParams ? searchParams.split(',') : [];
+
+  const searchSkillsConditions = searchSkills.map(skill => ilike(users.skills, `%${skill}%`));
 
   const [lengths, profiles] = await Promise.all([
     db.select({ count: sql<number>`count(*)` }).from(users),
@@ -27,6 +30,7 @@ const ProfileGrid = async ({ page, searchParams }: Props) => {
             or(
               ilike(users.name, `%${searchParams}%`),
               ilike(users.location, `%${searchParams}%`),
+              ...searchSkillsConditions
             ),
           )
           .limit(itemPerPage)
